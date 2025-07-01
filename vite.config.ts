d33656246e385395d9b619c7fd7c692cd9b4d382
B4rbelli <samuelbarbelli@hotmail.com>
2025-07-01 12:25:48 -0400

    atualizacao vite com base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CBComercial/', // 👈 caminho do repositório GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
